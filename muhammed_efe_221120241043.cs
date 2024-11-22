cc11d5212730f8916130da173b3b765d347729ce
muhammedefe6565 <medmuham115@gmail.com>
2024-11-22 11:46:52 +0300

    Add files via upload

---
+++ muhammed_efe_221120241043.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("öğrencinin ismi");
+string isim = Convert.ToString(Console.ReadLine());
+
+Console.WriteLine("öğrencinin soyismi");
+string soyisim = Convert.ToString(Console.ReadLine());
+
+Console.WriteLine("öğrencinin numarası");
+int numara  = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("öğrencinin telefon numarası");
+double telefon  = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("isim="+isim);
+
+Console.WriteLine("soyisim=" + soyisim);
+
+Console.WriteLine("okul numarası="+numara);
+
+Console.WriteLine("telefon numarası="+telefon);
+
